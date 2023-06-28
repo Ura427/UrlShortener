3fbcb96a05ddc71fbd8cd2452750ab18d712f437
Yura <urakost427@gmail.com>
2023-06-28 13:51:57 +0300

    moved files to public folder

--- index.js
+++
@@ -1,9 +0,0 @@
-
-//Button onclick event
-$("#shorten-btn").click(function() {
-    var inputText = $("#link-input").val();
-    console.log(inputText);
-    if (inputText !== "") {
-        alert("Ok");
-    }
-});
