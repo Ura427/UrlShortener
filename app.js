8eb09b495cc493f0b7d29c8f2c1068b4eb3f4ce8
Yura <urakost427@gmail.com>
2023-06-29 16:03:12 +0300

    fix bug which caused redirecting to last page only

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const crypto = require("crypto");
 const mongoose = require("mongoose");
 
 //modules
-//const index = require("./public/js/index");
 const mongooseDb = require("./mongooseDb"); 
 
 //app uses
@@ -16,27 +15,9 @@ app.use(bodyParser.urlencoded({ extended: true}));
 app.set('view engine', 'ejs');
 
 //global variables
-const links = {};//Store original links and their short versions
+//const links = {};//Store original links and their short versions
 var currShortLink = "";
 var currOriginalLink = "";
-
-
-//mongoose
-// mongoose.connect("mongodb://127.0.0.1:27017/links");
-// const Link = mongoose.model("Link", 
-// { 
-//     originalLink:{
-//         type: String,
-//         required: true,
-//         unique: true
-//     },
-//     shortenedLink:{
-//         type: String,
-//         required: true,
-//         unique: true
-//     } 
-    
-// });
 
 
 //Home GET
@@ -51,7 +32,7 @@ app.post("/", function(req, res){
 
     hashedText = fullUrl + HashString(inputText);
 
-    links[inputText] = hashedText;//Add hashed url in dictionary
+    //links[inputText] = hashedText;//Add hashed url in dictionary
 
     //Set current links
     currOriginalLink = inputText;
@@ -60,27 +41,31 @@ app.post("/", function(req, res){
 
     mongooseDb.AddToDb(currOriginalLink, currShortLink);
 
-    //mongoose
-    // const newLink = new Link({ originalLink: currOriginalLink, shortenedLink: currShortLink });
-    // newLink.save().then(() => console.log("Link saved successfully"));
-
     res.redirect("/short");
 })
 
 //Short GET
 app.get("/short", async function(req,res){
     var linkObject = await mongooseDb.Find(currShortLink);
-    //res.render("short", {shortLink: currShortLink, originalLink: currOriginalLink});
-    console.log(linkObject);
+    //console.log(linkObject);
     res.render("short", {shortLink: linkObject.shortenedLink, originalLink: linkObject.originalLink});
 })
 
 
 //Link GET
-app.get("/:link", function(req, res){
+app.get("/:link", async function(req, res){
+    var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl; //Get full page url
+    //console.log(fullUrl + req.params.link);
+    // res.writeHead(301, {
+    //     Location: currOriginalLink
+    //   }).end();
 
-    res.writeHead(301, {
-        Location: currOriginalLink
+     //var redUrl = fullUrl + "/" + req.params.link;
+     console.log(fullUrl);
+     var linkObject = await mongooseDb.Find(fullUrl);
+     console.log(linkObject);
+      res.writeHead(301, {
+        Location: linkObject.originalLink
       }).end();
 })
 
@@ -92,10 +77,6 @@ function HashString(text){
 
     return hashedText.slice(0,8);
 }
-
-// function getKeyByValue(object, value) {
-//     return Object.keys(object).find(key => object[key] === value);
-//   }
 
 //Connect to port
 app.listen(3000, function(){
