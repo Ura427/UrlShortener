b9a1f6a9b9a66051f9228b746198f5d04b9d116f
Yura <urakost427@gmail.com>
2023-06-29 12:43:15 +0300

    add comments and fixed formatting

--- app.js
+++ app.js
@@ -10,51 +10,46 @@ app.use(bodyParser.urlencoded({ extended: true}));
 
 app.set('view engine', 'ejs');
 
-const links = {};
+const links = {};//Store original links and their short versions
 var currShortLink = "";
 var currOriginalLink = "";
 
+//Home GET
 app.get("/", function(req, res){
-    res.sendFile(__dirname + "/index.html");
-   
-
+    res.sendFile(__dirname + "/index.html");   
 })
 
+//Home POST
 app.post("/", function(req, res){
-    const inputText = req.body.link;
-    var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+    const inputText = req.body.link;//Get input text value
+    var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl; //Get full page url
 
     hashedText = fullUrl + HashString(inputText);
 
-    links[inputText] = hashedText;
+    links[inputText] = hashedText;//Add hashed url in dictionary
 
+    //Set current links
     currOriginalLink = inputText;
     currShortLink = hashedText
 
     res.redirect("/short");
 })
 
-
+//Short GET
 app.get("/short", function(req,res){
-    // var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
-
     res.render("short", {shortLink: currShortLink, originalLink: currOriginalLink});
 })
 
 
-
+//Link GET
 app.get("/:link", function(req, res){
     //const originalLink = getKeyByValue(links, req.params.link);
-    // console.log("1:  " + originalLink);
-    // res.redirect(originalLink);
-    console.log("1:  " + currOriginalLink);
-    //res.redirect(currOriginalLink);
     res.writeHead(301, {
         Location: currOriginalLink
       }).end();
 })
 
-
+//Hashing string
 function HashString(text){
     const hash = crypto.createHash("sha256");
     hash.update(text);
@@ -67,6 +62,7 @@ function HashString(text){
 //     return Object.keys(object).find(key => object[key] === value);
 //   }
 
+//Connect to port
 app.listen(3000, function(){
     console.log("Server is running on port 3000");
 });
